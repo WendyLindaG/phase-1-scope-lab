982c603c953a0231151db5adcc2bf793c3f1b947
Wendy Linda Gakii <wendylinda2017@gmail.com>
2023-03-19 02:18:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavCustomer = "Karimi";
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+function setBestCustomer() {
+  bestCustomer = "not bob";
+  return bestCustomer;
+}
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+  return bestCustomer;
+}
+function changeLeastFavoriteCustomer() {
+  leastFavCustomer = "Pete";
+  return leastFavClient;
+}
